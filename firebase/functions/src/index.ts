23ad272d2101e34fb1c8c6ac0e3ff03bcf9901ed
Evy Bongers <evy@evybongers.nl>
2022-03-20 11:37:06 +0100

    Initial setup of firebase deploy

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,9 +1,28 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import { getMessaging } from "firebase-admin/messaging";
+
+initializeApp();
+
+interface AccountDocument {
+  devices: { name: string; token: string }[];
+}
+
+const db = getFirestore();
+export const sendNotifications = functions.region("europe-west1").https.onCall(
+  async (data, context) => {
+    let accounts = await db.collection("accounts").get();
+    for (let account of accounts.docs) {
+      let data = account.data() as AccountDocument;
+      let message = {
+        title: "lakshfd",
+        body: "Bunnie's awesome",
+        tokens: data.devices.map((_) => _.token),
+      };
+      let response = await getMessaging().sendMulticast(message);
+      console.log(response.successCount + " messages were sent successfully");
+    }
+  },
+);
