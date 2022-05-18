8e4eb79924a5a8b3af4123bf46fb29d1b3bf96df
Evy Bongers <evy@evybongers.nl>
2022-05-18 12:11:43 +0200

    Set up sending reminders

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,10 +1,39 @@
 import * as functions from "firebase-functions";
 
 import {initializeApp} from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
+import {getFirestore, FieldValue} from "firebase-admin/firestore";
 import {getMessaging} from "firebase-admin/messaging";
+import {firestore} from "firebase-admin";
+import DocumentReference = firestore.DocumentReference;
 
 initializeApp();
+
+
+const NOTIFICATION_TYPES: { [key: string]: { calculateNextSend: () => Date } } = {
+  //TODO write these
+  'hourly': {
+    calculateNextSend() {
+      let date = new Date();
+      date.setHours(date.getHours() + 1);
+      date.setMinutes(0);
+      date.setSeconds(0);
+      date.setMilliseconds(0);
+
+      return date;
+    },
+  },
+  'daily': {
+    calculateNextSend() {
+      let date = new Date();
+      date.setHours(date.getHours() + 24);
+      date.setMinutes(0);
+      date.setSeconds(0);
+      date.setMilliseconds(0);
+
+      return date;
+    },
+  },
+};
 
 interface AccountDevice {
   name: string;
@@ -13,6 +42,14 @@ interface AccountDevice {
 
 interface AccountDocument {
   devices: { [key: string]: AccountDevice };
+}
+
+interface AccountScheduledNotificationDocument {
+  title: string;
+  body: string;
+  nextSend: any;
+  lastSend: any;
+  type: string;
 }
 
 export const doSendNotifications = functions.region("europe-west1").https.onCall(
@@ -47,5 +84,33 @@ export const sendNotifications = functions.region("europe-west1")
       };
       let response = await getMessaging().sendMulticast(message);
       console.log(response.successCount + " messages were sent successfully");
+    }
+  });
+
+
+export const runNotify = functions.region("europe-west1")
+  .pubsub
+  .schedule("* * * * *")
+  .timeZone("Europe/Amsterdam")
+  .onRun(async _context => {
+    let scheduledNotifications = await db.collectionGroup("scheduledNotifications")
+      .where('nextSend', '<=', new Date())
+      .get();
+
+    console.log('scheduledNotifications', scheduledNotifications);
+
+    for (let scheduledNotification of scheduledNotifications.docs) {
+      let scheduledNotificationData = scheduledNotification.data() as AccountScheduledNotificationDocument;
+      console.log('creating', scheduledNotificationData);
+      let accountRef = scheduledNotification.ref.parent.parent as DocumentReference;
+      await accountRef.collection("notifications").add({
+        title: scheduledNotificationData.title,
+        body: scheduledNotificationData.body,
+      });
+
+      await scheduledNotification.ref.update({
+        lastSend: FieldValue.serverTimestamp(),
+        nextSend: NOTIFICATION_TYPES[scheduledNotificationData.type].calculateNextSend()
+      });
     }
   });
