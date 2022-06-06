22cb327eebe6dad48244e53c31e8f6615ef1674a
Evy Bongers <evy@evybongers.nl>
2022-06-06 12:12:45 +0200

    Import loadCollection

--- src/components/notification-preferences.ts
+++ src/components/notification-preferences.ts
@@ -1,7 +1,7 @@
 import {css, html, LitElement} from "lit";
 import {customElement, property} from "lit/decorators.js";
-import {collection, onSnapshot, query, where, getDocs} from "firebase/firestore";
-import {db, DataCollectionSupplier} from "../db";
+import {query, where, getDocs} from "firebase/firestore";
+import {DataCollectionSupplier, loadCollection} from "../db";
 import {renderItems} from "../helpers/Rendering";
 import './notification-preference-item'
 
@@ -33,39 +33,6 @@ export class NotificationPreferences extends LitElement {
     this.scheduledNotifications = this.loadCollection<{ test: number }>('accounts/QILOd8sLS6Z1Vtr7xuTJCeo4Si19/scheduledNotifications');
   }
 
-  async* loadCollection<T = any>(path: string): DataCollectionSupplier<T> {
-    let lastCallback: (docs: any[]) => void;
-    let nextItems = new Promise<T[]>((s) => {
-      lastCallback = s;
-    });
-
-    onSnapshot(collection(db, path), (snapshot) => {
-      lastCallback(snapshot.docs.map(_ => {
-        let data = _.data();
-        Object.defineProperty(data, '_ref', {value: _.ref});
-
-        return data;
-      }));
-
-      nextItems = new Promise<T[]>((s) => {
-        lastCallback = s;
-      });
-    });
-
-    while (1) {
-      let outputItems = await nextItems;
-      yield outputItems;
-    }
-  }
-
-  // async* loadDocument<T = any>(path: string): DataSupplier<T> {
-  //   yield {test: 69} as any;
-  //   await new Promise(_ => setTimeout(_, 1000))
-  //   yield {test: 3} as any;
-  //   await new Promise(_ => setTimeout(_, 1000))
-  //   yield {test: 342} as any;
-  //   await new Promise(_ => setTimeout(_, 1000))
-  // }
 }
 
 declare global {
