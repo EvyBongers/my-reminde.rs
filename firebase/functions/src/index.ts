abd84cc3d53f0bf6269f36a823be5ccc67245da1
Evy Bongers <evy@evybongers.nl>
2022-08-20 13:57:51 +0200

    Formatting

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,14 +1,13 @@
 import * as functions from "firebase-functions";
 
 import {initializeApp} from "firebase-admin/app";
-import {getFirestore, FieldValue} from "firebase-admin/firestore";
+import {FieldValue, getFirestore} from "firebase-admin/firestore";
 import {getMessaging} from "firebase-admin/messaging";
 import {firestore} from "firebase-admin";
 import {parseExpression} from "cron-parser-all";
 
 initializeApp();
 const messaging = getMessaging();
-
 
 interface AccountDevice {
   name: string;
@@ -32,18 +31,18 @@ export interface AccountScheduledNotificationDocument {
 }
 
 const calculateNextSend = (notification: AccountScheduledNotificationDocument) => {
-    switch (notification.type) {
-        case "cron": {
-            // TODO(ebongers): use preference in user profile
-            let options = {tz: "Europe/Amsterdam"};
-            let cron = parseExpression(notification.cronExpression as string, options);
-            return cron.next().toDate();
-        }
-        default: {
-            functions.logger.error(`Unknown notification type ${notification.type} on document ${notification.ref}`);
-            return null;
-        }
+  switch (notification.type) {
+    case "cron": {
+      // TODO(ebongers): use preference in user profile
+      let options = {tz: "Europe/Amsterdam"};
+      let cron = parseExpression(notification.cronExpression as string, options);
+      return cron.next().toDate();
     }
+    default: {
+      functions.logger.error(`Unknown notification type ${notification.type} on document ${notification.ref}`);
+      return null;
+    }
+  }
 };
 
 const db = getFirestore();
@@ -73,94 +72,93 @@ let getPushTokens = (account: AccountDocument) => {
 };
 
 export const updateNextSend = functions.region("europe-west1")
-  .firestore.document("/accounts/{accountId}/scheduledNotifications/{notificationId}")
-  .onWrite(async (change, context) => {
-    let oldNotification = change.before;
-    let oldNotificationData = oldNotification.data() as AccountScheduledNotificationDocument;
+.firestore.document("/accounts/{accountId}/scheduledNotifications/{notificationId}")
+.onWrite(async (change, context) => {
+  let oldNotification = change.before;
+  let oldNotificationData = oldNotification.data() as AccountScheduledNotificationDocument;
 
-    let notification = change.after;
-    let notificationData = notification.data() as AccountScheduledNotificationDocument;
+  let notification = change.after;
+  let notificationData = notification.data() as AccountScheduledNotificationDocument;
 
-    if (notificationData.enabled === false) return;
+  if (notificationData.enabled === false) return;
 
-    try {
-          let nextSend = calculateNextSend(notificationData);
-          if (nextSend != oldNotificationData.nextSend) {
-            functions.logger.debug(`Updating timestamps on notification ${notification.ref.id}:`, {nextSend: nextSend});
-            await notification.ref.update({nextSend: nextSend});
-          }
-    } catch (e) {
-      functions.logger.error(`Failed to update nextSend on document ${notification.ref}`);
-      functions.logger.error((e as Error).message);
+  try {
+    let nextSend = calculateNextSend(notificationData);
+    if (nextSend != oldNotificationData.nextSend) {
+      functions.logger.debug(`Updating timestamps on notification ${notification.ref.id}:`, {nextSend: nextSend});
+      await notification.ref.update({nextSend: nextSend});
     }
-  });
+  } catch (e) {
+    functions.logger.error(`Failed to update nextSend on document ${notification.ref}`);
+    functions.logger.error((e as Error).message);
+  }
+});
 
 export const sendNotifications = functions.region("europe-west1")
-  .firestore.document("/accounts/{accountId}/notifications/{notificationId}")
-  .onCreate(async (snapshot, context) => {
-    let notificationData = snapshot.data();
-    let accounts = await db.collection("accounts").get();
-    for (let account of accounts.docs) {
-      let accountData = account.data() as AccountDocument;
-      let _notification = {
-        title: notificationData.title,
-        body: notificationData.body,
-      };
-      let batchResponse = await messaging.sendMulticast({
-        "notification": _notification,
-        "webpush": {
-          "notification": {
-            ..._notification,
-            // "actions": [
-            //   {
-            //     "title": "OK",
-            //     "action": "void()",
-            //   },
-            //   {
-            //     "title": "Dismiss",
-            //     "action": "void()",
-            //   },
-            // ],
-            "renotify": true,
-            "requireInteraction": true,
-            "tag": snapshot.id,
-            "timestamp": (notificationData.sent as firestore.Timestamp).toMillis(),
-          },
-          // "fcmOptions": notificationData.link ? {
-          //   "link": notificationData.link,
-          // } : {},
+.firestore.document("/accounts/{accountId}/notifications/{notificationId}")
+.onCreate(async (snapshot, context) => {
+  let notificationData = snapshot.data();
+  let accounts = await db.collection("accounts").get();
+  for (let account of accounts.docs) {
+    let accountData = account.data() as AccountDocument;
+    let _notification = {
+      title: notificationData.title,
+      body: notificationData.body,
+    };
+    let batchResponse = await messaging.sendMulticast({
+      "notification": _notification,
+      "webpush": {
+        "notification": {
+          ..._notification,
+          // "actions": [
+          //   {
+          //     "title": "OK",
+          //     "action": "void()",
+          //   },
+          //   {
+          //     "title": "Dismiss",
+          //     "action": "void()",
+          //   },
+          // ],
+          "renotify": true,
+          "requireInteraction": true,
+          "tag": snapshot.id,
+          "timestamp": (notificationData.sent as firestore.Timestamp).toMillis(),
         },
-        "tokens": getPushTokens(accountData),
+        // "fcmOptions": notificationData.link ? {
+        //   "link": notificationData.link,
+        // } : {},
+      },
+      "tokens": getPushTokens(accountData),
+    });
+    functions.logger.info(batchResponse.successCount + " messages were sent successfully");
+    if (batchResponse.failureCount > 0) {
+      functions.logger.error(batchResponse.failureCount + " messages failed to send");
+      batchResponse.responses.filter(response => !response.success).forEach(response => {
+        functions.logger.debug(response.messageId, response.error);
       });
-      functions.logger.info(batchResponse.successCount + " messages were sent successfully");
-      if (batchResponse.failureCount > 0) {
-        functions.logger.error(batchResponse.failureCount + " messages failed to send");
-        batchResponse.responses.filter(response => !response.success).forEach(response => {
-          functions.logger.debug(response.messageId, response.error);
-        });
-      }
     }
-  });
-
+  }
+});
 
 export const runNotify = functions.region("europe-west1")
-  .pubsub
-  .schedule("* * * * *")
-  .timeZone("Europe/Amsterdam")
-  .onRun(async _context => {
-    let scheduledNotifications = await db.collectionGroup("scheduledNotifications")
-      .where("nextSend", "<=", new Date())
-      .where("enabled", "==", true)
-      .get();
+.pubsub
+.schedule("* * * * *")
+.timeZone("Europe/Amsterdam")
+.onRun(async _context => {
+  let scheduledNotifications = await db.collectionGroup("scheduledNotifications")
+  .where("nextSend", "<=", new Date())
+  .where("enabled", "==", true)
+  .get();
 
-    for (let scheduledNotification of scheduledNotifications.docs) {
-      let scheduledNotificationData = scheduledNotification.data() as AccountScheduledNotificationDocument;
-      let nextSend = calculateNextSend(scheduledNotificationData);
+  for (let scheduledNotification of scheduledNotifications.docs) {
+    let scheduledNotificationData = scheduledNotification.data() as AccountScheduledNotificationDocument;
+    let nextSend = calculateNextSend(scheduledNotificationData);
 
-      functions.logger.info("creating", scheduledNotificationData);
-      await triggerNotification(scheduledNotification.ref, scheduledNotificationData);
+    functions.logger.info("creating", scheduledNotificationData);
+    await triggerNotification(scheduledNotification.ref, scheduledNotificationData);
 
-      functions.logger.debug(`Updating timestamps on notification ${scheduledNotification.ref.id}:`, {nextSend: nextSend});
-      await scheduledNotification.ref.update({lastSent: FieldValue.serverTimestamp(), nextSend: nextSend});
-    }
-  });
+    functions.logger.debug(`Updating timestamps on notification ${scheduledNotification.ref.id}:`, {nextSend: nextSend});
+    await scheduledNotification.ref.update({lastSent: FieldValue.serverTimestamp(), nextSend: nextSend});
+  }
+});
