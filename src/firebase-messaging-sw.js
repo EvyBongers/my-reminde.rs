e58d1af9184ea1e269eae0e0d2d2a0475680b5a5
Evy Bongers <evy@evybongers.nl>
2022-11-08 13:14:01 +0100

    Have service worker use modules

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,7 +1,7 @@
-importScripts("https://www.gstatic.com/firebasejs/9.6.8/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.6.8/firebase-messaging-compat.js");
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
 
-const firebaseApp = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyAQeo7TxkZXoPZlB31rnbdJ4YK6J9l3lHY",
   authDomain: "qvyldr.firebaseapp.com",
   projectId: "qvyldr",
@@ -9,10 +9,13 @@ const firebaseApp = firebase.initializeApp({
   messagingSenderId: "452277637486",
   appId: "1:452277637486:web:afa0d040adf5c183eef17f",
   measurementId: "G-HYCWSGGJCF",
-});
+};
 
-// Retrieve an instance of Firebase Messaging so that it can handle background messages.
-const messaging = firebase.messaging(firebaseApp);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Cloud Messaging and get a reference to the service
+const messaging = getMessaging(app);
 
 self.addEventListener('notificationclick', (event) => {
   console.log("Notification click registered", event);
