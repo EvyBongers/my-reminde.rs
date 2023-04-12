ccbddc21c83cbd95eae975118d552d2dd2a6cd01
Evy Bongers <evy@evybongers.nl>
2023-04-12 08:31:02 +0200

    Add stuff to service worker

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -16,10 +16,34 @@ const app = firebase.initializeApp(firebaseConfig);
 // Initialize Firebase Cloud Messaging and get a reference to the service
 const messaging = firebase.messaging(app);
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener('notificationclick', (event) => {
   console.log("Notification click registered", event);
   let data = event.notification.data.FCM_MSG?.data ?? event.notification.data;
   let url = event.action || `${location.protocol}//${location.host}/notifications/${data.notificationId}`;
+
+  for (let _type of ["window", "worker", "sharedworker", "all"]) {
+    for (let _includeUncontrolled of [false, true]) {
+      let options = {
+        type: _type,
+        includeUncontrolled: _includeUncontrolled,
+      }
+      console.log("Options:", options);
+      self.clients.matchAll(options).then((clientList) => {
+        console.log("Matching clients:", clientList);
+        // for (const client of clientList) {
+        //   if (client.url === "index.html") {
+        //     clients.openWindow(client);
+        //     // or do something else involving the matching client
+        //   }
+        // }
+      });
+    }
+  }
+
   console.log(`Opening url: ${url}`);
   self.clients.openWindow(url);
   event.notification.close();
