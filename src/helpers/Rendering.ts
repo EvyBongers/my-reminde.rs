4f1bbbacc76e59b659ded7944f1a8cb6879d338b
Evy Bongers <evy@evybongers.nl>
2023-05-01 16:02:47 +0200

    Add render options for loading and no data

--- src/helpers/Rendering.ts
+++ src/helpers/Rendering.ts
@@ -1,5 +1,10 @@
+import {TemplateResult} from "lit";
 import {asyncReplace} from "lit/directives/async-replace.js";
-import {TemplateResult} from "lit/development";
+
+export type renderOptions = {
+  loading?: TemplateResult
+  placeHolder?: TemplateResult
+}
 
 export const navigate = (
   route: string,
@@ -15,22 +20,22 @@ export const navigate = (
   return defaultCase;
 };
 
-export const renderItems = (items: AsyncGenerator<any[]>, render: (item: any, index: number) => any, loading?: any) => {
-  if(!items) return loading;
+export const renderItems = (items: AsyncGenerator<any[]>, render: (item: any, index: number) => any, options?: renderOptions | any) => {
+  if (!items) return options?.loading ? options.loading : options;
 
   return asyncReplace((async function* () {
     for await(let subItems of items) {
-      yield subItems.map((item, index) => render(item, index));
+      yield (subItems.length === 0 && options?.placeHolder) ? options.placeHolder : subItems.map((item, index) => render(item, index));
     }
   })());
 };
 
-export const renderItem = (items: AsyncGenerator<any>, render: (item: any) => any, loading?: any) => {
-  if(!items) return loading;
+export const renderItem = (items: AsyncGenerator<any>, render: (item: any) => any, options?: renderOptions | any) => {
+  if (!items) return options?.loading ? options.loading : options;
 
   return asyncReplace((async function* () {
     for await(let item of items) {
-      yield render(item);
+      yield (!item && options?.placeHolder) ? options.placeHolder : render(item);
     }
   })());
 };
