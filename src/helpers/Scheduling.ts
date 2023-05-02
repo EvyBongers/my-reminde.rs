c299d7325357888af4de753313941301d8f4e5d9
Evy Bongers <evy@evybongers.nl>
2023-05-02 20:37:24 +0200

    Return early when schedule is undefined

--- src/helpers/Scheduling.ts
+++ src/helpers/Scheduling.ts
@@ -6,6 +6,8 @@ export const calculateNextSend = (notification: Partial<ReminderDocument>) => {
 
     switch (notification.type) {
         case "cron": {
+            if (notification.cronExpression === undefined) return
+
             // TODO(ebongers): use preference in user profile
             let options = {tz: "Europe/Amsterdam"};
             let cron = parseExpression(notification.cronExpression as string, options);
