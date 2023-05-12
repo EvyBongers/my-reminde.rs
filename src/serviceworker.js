5e5bd0fd0c311fb114ca8d5ae32c21f3a1726383
Evy Bongers <evy@evybongers.nl>
2023-05-12 18:58:41 +0200

    Clean up service worker

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -19,22 +19,22 @@ const CACHE_NAME = 'offline';
 const OFFLINE_URL = 'offline.html';
 
 self.addEventListener('install', (event) => {
-  event.waitUntil((async () => {
+  event.waitUntil(async () => {
     const cache = await caches.open(CACHE_NAME);
     // Setting {cache: 'reload'} in the new request will ensure that the response
     // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
     await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-  })());
+  });
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
+  event.waitUntil(async () => {
     // Enable navigation preload if it's supported.
     // See https://developers.google.com/web/updates/2017/02/navigation-preload
     if ('navigationPreload' in self.registration) {
       await self.registration.navigationPreload.enable();
     }
-  })());
+  });
 
   // Tell the active service worker to take control of the page immediately.
   self.clients.claim();
@@ -44,7 +44,7 @@ self.addEventListener('fetch', (event) => {
   // We only want to call event.respondWith() if this is a navigation request
   // for an HTML page.
   if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
+    event.respondWith(async () => {
       try {
         // First, try to use the navigation preload response if it's supported.
         const preloadResponse = await event.preloadResponse;
@@ -65,12 +65,6 @@ self.addEventListener('fetch', (event) => {
         const cachedResponse = await cache.match(OFFLINE_URL);
         return cachedResponse;
       }
-    })());
+    });
   }
-
-  // If our if() condition is false, then this fetch handler won't intercept the
-  // request. If there are any other fetch handlers registered, they will get a
-  // chance to call event.respondWith(). If no fetch handlers call
-  // event.respondWith(), the request will be handled by the browser as if there
-  // were no service worker involvement.
 });
