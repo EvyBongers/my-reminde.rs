9615b6cd8e09e13525b4ae8e1ba4bc89b35acb32
Evy Bongers <evy@evybongers.nl>
2023-05-15 08:35:14 +0200

    Move service worker fetch to named function

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -49,30 +49,35 @@ self.addEventListener('fetch', (event) => {
   // We only want to call event.respondWith() if this is a navigation request
   // for an HTML page.
   if (event.request.mode === 'navigate') {
-    event.respondWith(async () => {
-      try {
-        // First, try to use the navigation preload response if it's supported.
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
-
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-        // catch is only triggered if an exception is thrown, which is likely
-        // due to a network error.
-        // If fetch() returns a valid HTTP response with a response code in
-        // the 4xx or 5xx range, the catch() will NOT be called.
-        console.log('Fetch failed; returning offline page instead.', error);
-
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
-      }
-    });
+    event.respondWith(doFetch(event));
   }
 });
+
+async function doFetch(event) {
+  try {
+    // First, try to use the navigation preload response if it's supported.
+    const preloadResponse = await event.preloadResponse;
+    if (preloadResponse) {
+      console.log("Responding with preload:", preloadResponse);
+      return preloadResponse;
+    }
+
+    const networkResponse = await fetch(event.request);
+    console.log("Responding with network:", networkResponse);
+    return networkResponse;
+  } catch (error) {
+    // catch is only triggered if an exception is thrown, which is likely
+    // due to a network error.
+    // If fetch() returns a valid HTTP response with a response code in
+    // the 4xx or 5xx range, the catch() will NOT be called.
+    console.log('Fetch failed; returning offline page instead.', error);
+
+    const cache = await caches.open(CACHE_NAME);
+    const cachedResponse = await cache.match(OFFLINE_URL);
+    console.log("Responding with cache:", cachedResponse);
+    return cachedResponse;
+  }
+}
 
 self.addEventListener('notificationclick', (event) => {
   console.log("Notification click registered", event);
