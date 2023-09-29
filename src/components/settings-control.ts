4a0a25ab26cd29eab9496e776f2dc0c2472bc4e4
Evy Bongers <evy@evybongers.nl>
2023-09-29 12:28:22 +0200

    Change device renaming to prompt

--- src/components/settings-control.ts
+++ src/components/settings-control.ts
@@ -1,7 +1,9 @@
 import {css, html, LitElement, render} from "lit";
-import {customElement, property} from "lit/decorators.js";
+import {customElement, property, query} from "lit/decorators.js";
 import {SingleSelectedEvent} from "@material/mwc-list";
 import "@material/mwc-formfield";
+import "@material/mwc-icon-button";
+import "@material/mwc-select";
 import "@material/mwc-switch";
 import {ReminderDocument} from "../../firebase/functions/src";
 import {showMessage} from "../helpers/Snacks";
@@ -11,17 +13,24 @@ import {disablePushNotifications, enablePushNotifications, isPushNotificationsEn
 import {getDeviceId, getDeviceName, setDeviceName} from "../helpers/Device";
 import {updateAccount} from "../auth";
 import {BunnyElement, observe} from "./bunny-element";
+import {ConfirmDialog} from "./confirm-dialog";
+import {choose} from "lit/directives/choose.js";
 
 @customElement("settings-control")
 export class SettingsControl extends BunnyElement {
   _deviceName: string = getDeviceName();
   timezone: string;
 
+  settingEditing: string;
+
   @property()
   accountId: string;
 
   @property()
   pushNotificationsEnabled: boolean;
+
+  @query("confirm-dialog")
+  settingsDialog: ConfirmDialog;
 
   @property()
   get deviceName(): string {
@@ -40,10 +49,20 @@ export class SettingsControl extends BunnyElement {
       flex-direction: column;
     }
 
+    confirm-dialog > * {
+      width: 100%;
+    }
+
     mwc-formfield {
       height: 56px;
       display: flex;
       width: 100%;
+    }
+
+    mwc-formfield div {
+      display: inline-flex;
+      align-items: center;
+      vertical-align: middle;
     }
 
     mwc-formfield mwc-switch {
@@ -66,9 +85,10 @@ export class SettingsControl extends BunnyElement {
           <mwc-switch ?selected="${this.pushNotificationsEnabled}"></mwc-switch>
         </mwc-formfield>
         <mwc-formfield label="Device name" alignEnd spaceBetween>
-          <mwc-textfield type="text" name="device" .value="${this.deviceName}" placeholder="${navigator.userAgent}"
-                         @input="${(_: Event) => this.deviceName = (_.currentTarget as HTMLInputElement).value}">
-          </mwc-textfield>
+          <div>
+            <span>${this.deviceName}</span>
+            <mwc-icon-button icon="navigate_next" @click="${this.promptSetting}" data-setting-name="deviceName"></mwc-icon-button>
+          </div>
         </mwc-formfield>
         <mwc-formfield label="Notifications timezone" alignEnd spaceBetween>
           <mwc-select name="timezone" .value="${this.timezone}"
@@ -78,7 +98,26 @@ export class SettingsControl extends BunnyElement {
         </mwc-formfield>
         <mwc-button outlined icon="send" @click="${this.sendNotification}">Send a test notification</mwc-button>
       </div>
+
+      <confirm-dialog confirmLabel="Save" cancelLabel="Cancel">
+        ${choose(this.settingEditing, [
+          ["deviceName", () => html`
+            <mwc-textfield label="Device name" type="text" name="device" .value="${this.deviceName}" placeholder="${navigator.userAgent}" @input="${(_: Event) => this.deviceName = (_.currentTarget as HTMLInputElement).value}"></mwc-textfield>
+          `],
+          ], () => html`Unknown setting '${this.settingEditing}'`)}
+      </confirm-dialog>
     `;
+  }
+
+  private promptSetting(ev: Event) {
+    ev.stopPropagation();
+    (ev.target as HTMLElement).blur();
+
+    const eventDataset = (ev.currentTarget as HTMLElement | LitElement).dataset;
+    this.settingEditing = eventDataset.settingName;
+    console.log(this.settingEditing);
+    this.requestUpdate("settingEditing", undefined);
+    this.settingsDialog.show();
   }
 
   private async loadPushNotificationsState() {
